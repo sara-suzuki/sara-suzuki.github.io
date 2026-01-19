2fef6aa24e964e2f63da64f9f659a8351263ac11
sara-suzuki <92958323+sara-suzuki@users.noreply.github.com>
2026-01-19 09:48:30 -0500

    pull quote on about me page

--- js/config.js
+++ js/config.js
@@ -34,8 +34,10 @@ const CONFIG = {
     bio: [
       "My research focuses on uncovering and disrupting inequities in youth civic engagement. I consider all forms of change-making, including voting, running for office, community organizing, and service. I look at the pathways that young people have to tackle the issues that they care about and investigate shifts—individual, organizational, institutional, and system-wide—that can bring greater inclusion to those pathways.",
       "I believe that young people have the power to enact sociopolitical praxis (critical consciousness) to challenge systems of oppression and bring about social change. I strive to use my skills of quantitative, qualitative, and mixed methods research, facilitation, organizational capacity-building, and leadership development to learn from and with young people about how they are creating a more equitable future.",
+      "{{pullquote}}Building a more just democracy through co-creating knowledge and power with young people.{{/pullquote}}",
       "I currently work as a Senior Researcher at CIRCLE, a non-partisan research center that is part of the Tisch College of Civic Life at Tufts University. I received my Ph.D. in Applied Developmental and Educational Psychology from Boston College."
     ],
+    // Profile image (place in /images/ folder)
     image: "images/profile.jpg"
   },
 
